dcb28cc09160c41879a6a0351c010e0aaccf5ed4
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-18 14:11:15 +0300

    Выполнено задание №1

--- add_new_group.py
+++ add_new_group.py
@@ -7,17 +7,25 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
+def is_alert_present(self):
+    try:
+        self.driver.switch_to_alert()
+    except NoAlertPresentException as e:
+        return False
+    return True
+
+
 class AddNewGroup(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
     def test_add_new_group(self):
         driver = self.driver
-        driver.get(self.base_url + "/addressbook/")
+        driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("")
         driver.find_element_by_name("user").clear()
@@ -25,7 +33,7 @@ class AddNewGroup(unittest.TestCase):
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("secret")
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        time.sleep(5)
+        time.sleep(0.5)
         driver.find_element_by_link_text("groups").click()
         driver.find_element_by_xpath("(//input[@name='new'])[2]").click()
         driver.find_element_by_name("group_name").clear()
@@ -36,20 +44,16 @@ class AddNewGroup(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys("qwerty")
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
-        #driver.find_element_by_link_text("Logout").click()
-        #driver.find_element_by_name("user").clear()
-        #driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_link_text("Logout").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
