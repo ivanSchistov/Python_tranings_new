3bd358de0c5cd7ebb4af8f98da1de4ac44d482a1
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-20 18:40:06 +0300

    Добавлена 3-я домашка

--- add_new_contact.py
+++ add_new_contact.py
@@ -6,87 +6,118 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
+
+
+def is_alert_present(self):
+    try:
+        self.driver.switch_to_alert()
+    except NoAlertPresentException as e:
+        return False
+    return True
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_add_new_contact(self):
-        driver = self.driver
-        driver.get(self.base_url + "/addressbook/")
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
+    def login(self, driver, username, password):
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("")
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+
+    def open_contact_page(self, driver):
         driver.find_element_by_link_text("add new").click()
+
+    def fill_contact_form(self, driver, contact):
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("rer")
+        driver.find_element_by_name("firstname").send_keys(contact.firstname)
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("qwer")
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("John")
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Smith")
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
         driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys("JS")
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
         driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys("company")
+        driver.find_element_by_name("title").send_keys(contact.title)
         driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("copm")
+        driver.find_element_by_name("company").send_keys(contact.company)
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("qwe12 33")
+        driver.find_element_by_name("address").send_keys(contact.address)
         driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("123456")
+        driver.find_element_by_name("home").send_keys(contact.home)
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("1234568")
+        driver.find_element_by_name("mobile").send_keys(contact.mobile)
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("123@ro.ru")
+        driver.find_element_by_name("email").send_keys(contact.email)
         driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys("www.site.com")
+        driver.find_element_by_name("homepage").send_keys(contact.homepage)
         Select(driver.find_element_by_name("bday")).select_by_visible_text("4")
         Select(driver.find_element_by_name("bmonth")).select_by_visible_text("April")
         driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1990")
+        driver.find_element_by_name("byear").send_keys(contact.byear)
         driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys("123")
+        driver.find_element_by_name("work").send_keys(contact.work)
         driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys("1-123-45")
+        driver.find_element_by_name("fax").send_keys(contact.fax)
         driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys("145@gmail.com")
+        driver.find_element_by_name("email2").send_keys(contact.email2)
         driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys("qwerty@gmail.com")
+        driver.find_element_by_name("email3").send_keys(contact.email3)
         Select(driver.find_element_by_name("aday")).select_by_visible_text("5")
         Select(driver.find_element_by_name("amonth")).select_by_visible_text("March")
         driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys("1992")
+        driver.find_element_by_name("ayear").send_keys(contact.ayear)
         driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys("qwasd12 22")
+        driver.find_element_by_name("address2").send_keys(contact.address2)
         driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys("Town")
+        driver.find_element_by_name("phone2").send_keys(contact.phone2)
         driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys("zxcv")
-        # ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+        driver.find_element_by_name("notes").send_keys(contact.notes)
+
+    def return_to_home_page(self, driver):
         driver.find_element_by_link_text("home page").click()
+
+    def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-    
+
+    def add_new_contact(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_contact_page(driver)
+        self.fill_contact_form(driver, Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
+                                               address='123', home='12456', mobile='123', email='123@ro.ru', homepage='www.site.com',bday='10',
+                                               bmonth='April', byear='1990', work='123', fax='1456987', email2='dad@gmail.com',
+                                               email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!'))
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
+    def add_new_empty_contact(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_contact_page(driver)
+        self.fill_contact_form(driver, Contact(firstname='', middlename='', lastname='', nickname='', title=''))
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+
     
     def close_alert_and_get_its_text(self):
         try:
