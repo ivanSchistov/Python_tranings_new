aac2ac2d05e6673bd7f41a8817bb915922e38724
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-25 19:08:23 +0300

    Созданы пакеты для разных зон ответственности

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.webdriver.common.by import By
 import unittest
-from contact import Contact
+
+from model.contact import Contact
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
 def is_alert_present(self):
@@ -22,10 +23,13 @@ class AddNewContact(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def open_home_page(self, driver):
+    def open_home_page(self):
+        driver = self.driver
+        self.open_home_page()
         driver.get("http://localhost/addressbook/")
 
-    def login(self, driver, username, password):
+    def login(self, username, password):
+        driver = self.driver
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("")
         driver.find_element_by_name("user").clear()
@@ -35,10 +39,12 @@ class AddNewContact(unittest.TestCase):
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
 
-    def open_contact_page(self, driver):
+    def open_contact_page(self):
+        driver = self.driver
         driver.find_element_by_link_text("add new").click()
 
-    def fill_contact_form(self, driver, contact):
+    def fill_contact_form(self, contact):
+        driver = self.driver
         driver.find_element_by_name("firstname").clear()
         driver.find_element_by_name("firstname").send_keys(contact.firstname)
         driver.find_element_by_name("middlename").clear()
@@ -85,35 +91,33 @@ class AddNewContact(unittest.TestCase):
         driver.find_element_by_name("notes").send_keys(contact.notes)
         driver.find_element(By.XPATH, ("//input[@value='Enter']")).click()
 
-    def return_to_home_page(self, driver):
+    def return_to_home_page(self):
+        driver = self.driver
         driver.find_element_by_link_text("home page").click()
 
-    def logout(self, driver):
+    def logout(self):
+        driver = self.driver
         driver.find_element_by_link_text("Logout").click()
 
     def test_add_new_contact(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_contact_page(driver)
-        self.fill_contact_form(driver, Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
+        self.login(username="admin", password="secret")
+        self.open_contact_page()
+        self.fill_contact_form(Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
                                                address='123', home='12456', mobile='123', email='123@ro.ru', homepage='www.site.com',bday='10',
                                                bmonth='April', byear='1990', work='123', fax='1456987', email2='dad@gmail.com',
                                                email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!'))
-        self.return_to_home_page(driver)
-        self.logout(driver)
+        self.return_to_home_page()
+        self.logout()
 
     def test_add_new_empty_contact(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_contact_page(driver)
-        self.fill_contact_form(driver, Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
+        self.login(username="admin", password="secret")
+        self.open_contact_page()
+        self.fill_contact_form(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
                                                address='', home='', mobile='', email='', homepage='',bday='',
                                                bmonth='', byear='', work='', fax='', email2='',
                                                email3='', aday='', amonth='', ayear='', address2='', phone2='', notes=''))
-        self.return_to_home_page(driver)
-        self.logout(driver)
+        self.return_to_home_page()
+        self.logout()
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
@@ -135,7 +139,6 @@ class AddNewContact(unittest.TestCase):
     
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
