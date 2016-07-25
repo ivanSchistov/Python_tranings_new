472bd03ebe4291a5bfba6ee9e823c92b42b7604f
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-25 20:17:07 +0300

    Обновлен скрипт для создания контактов, с использованием фикстуры

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -1,144 +1,30 @@
 # -*- coding: utf-8 -*-
-import unittest
-
+import pytest
 from model.contact import Contact
-from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from fixture.application import Application
 
 
-def is_alert_present(self):
-    try:
-        self.driver.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class AddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.verificationErrors = []
-        self.accept_next_alert = True
+def test_add_new_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.open_contact_page()
+    app.create(Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
+                       address='123', home='12456', mobile='123', email='123@ro.ru', homepage='www.site.com', bday='10',
+                       bmonth='April', byear='1990', work='123', fax='1456987', email2='dad@gmail.com',
+                       email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!'))
+    app.session.logout()
 
-    def open_home_page(self):
-        driver = self.driver
-        self.open_home_page()
-        driver.get("http://localhost/addressbook/")
+def test_add_new_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.open_contact_page()
+    app.create(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
+                       address='', home='', mobile='', email='', homepage='', bday='',
+                       bmonth='', byear='', work='', fax='', email2='',
+                       email3='', aday='', amonth='', ayear='', address2='', phone2='', notes=''))
+    app.session.logout()
 
-    def login(self, username, password):
-        driver = self.driver
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-
-    def open_contact_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("add new").click()
-
-    def fill_contact_form(self, contact):
-        driver = self.driver
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(contact.title)
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(contact.company)
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(contact.address)
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.home)
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys(contact.mobile)
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
-        driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("4")
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("April")
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys(contact.byear)
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(contact.work)
-        driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys(contact.fax)
-        driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys(contact.email2)
-        driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys(contact.email3)
-        Select(driver.find_element_by_name("aday")).select_by_visible_text("5")
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text("March")
-        driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys(contact.ayear)
-        driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys(contact.address2)
-        driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys(contact.phone2)
-        driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys(contact.notes)
-        driver.find_element(By.XPATH, ("//input[@value='Enter']")).click()
-
-    def return_to_home_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("home page").click()
-
-    def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
-
-    def test_add_new_contact(self):
-        self.login(username="admin", password="secret")
-        self.open_contact_page()
-        self.fill_contact_form(Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
-                                               address='123', home='12456', mobile='123', email='123@ro.ru', homepage='www.site.com',bday='10',
-                                               bmonth='April', byear='1990', work='123', fax='1456987', email2='dad@gmail.com',
-                                               email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!'))
-        self.return_to_home_page()
-        self.logout()
-
-    def test_add_new_empty_contact(self):
-        self.login(username="admin", password="secret")
-        self.open_contact_page()
-        self.fill_contact_form(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
-                                               address='', home='', mobile='', email='', homepage='',bday='',
-                                               bmonth='', byear='', work='', fax='', email2='',
-                                               email3='', aday='', amonth='', ayear='', address2='', phone2='', notes=''))
-        self.return_to_home_page()
-        self.logout()
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main()
