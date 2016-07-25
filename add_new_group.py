b4b0606244cc4028a8a4e5bb6814d7b80e6f32fd
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-25 18:48:56 +0300

    Обновленный тест групп с использованием фикстуры

--- add_new_group.py
+++ add_new_group.py
@@ -1,100 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from group import Group
-import unittest, time
+import time
+import pytest
+from application import Application
 
 
-def is_alert_present(self):
-    try:
-        self.driver.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-
-class AddNewGroup(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/")
-
-    def login(self, driver, username, password):
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def open_group_page(self, driver):
-        driver.find_element_by_link_text("groups").click()
-
-    def create_group(self, driver, group):
-        # create new group
-        driver.find_element_by_xpath("(//input[@name='new'])[2]").click()
-        # fill group fields
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.group_name)
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        driver.find_element_by_name("submit").click()
-
-    def return_group_page(self, driver):
-        driver.find_element_by_link_text("group page").click()
-
-    def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
-    def test_add_new_group(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
+def test_add_new_group(app):
+        app.login(username="admin", password="secret")
         time.sleep(0.5)
-        self.open_group_page(driver)
-        self.create_group(driver, Group(group_name="test_group", header="qwerty", footer="qwerty"))
-        self.return_group_page(driver)
-        self.logout(driver)
+        app.open_group_page()
+        app.create_group(Group(group_name="test_group", header="qwerty", footer="qwerty"))
+        app.logout()
 
-    def test_add_new_empty_group(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        time.sleep(0.5)
-        self.open_group_page(driver)
-        self.create_group(driver, Group(group_name="", header="", footer=""))
-        self.return_group_page(driver)
-        self.logout(driver)
+def test_add_new_empty_group(app):
+        app.login(username="admin", password="secret")
+        app.open_group_page()
+        app.create_group(Group(group_name="", header="", footer=""))
+        app.logout()
 
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
