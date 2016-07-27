aee9372693e09a871154aac47981cbfd294ca790
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-27 19:11:24 +0300

    Создан новый скрипт для редактирования группы

---
+++ test/test_modify_group.py
@@ -0,0 +1,6 @@
+__author__ = 'ichistov'
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group()
+    app.session.logout()
