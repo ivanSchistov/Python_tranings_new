78bbd05b02e85e235100b4a126f9cbcc65aedb77
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-27 18:45:56 +0300

    Создана общая для всех фикстура

---
+++ conftest.py
@@ -0,0 +1,12 @@
+__author__ = 'ichistov'
+
+import pytest
+from fixture.application import Application
+
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
