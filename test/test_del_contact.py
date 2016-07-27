b23cd7e9aa2d2afaf99438eb37eba60de7fe5231
ivanSchistov <ivan.s.chistov@gmail.com>
2016-07-27 20:05:28 +0300

    Добавлен тест на удаления контакта

---
+++ test/test_del_contact.py
@@ -0,0 +1,6 @@
+__author__ = 'ichistov'
+
+def test_delete_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.delete_first_contact()
+    app.session.logout()
