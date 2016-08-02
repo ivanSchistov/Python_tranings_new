449c488b48b7ee9caec66dedff056e2554565e1d
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-02 19:23:11 +0300

    апдейт скрипта

--- test/add_new_group.py
+++ test/add_new_group.py
@@ -4,11 +4,11 @@ from model.group import Group
 
 def test_add_new_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(group_name="test_group", header="qwerty", footer="qwerty"))
+    app.group.create(Group(name="test_group", header="qwerty", footer="qwerty"))
     app.session.logout()
 
 def test_add_new_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(group_name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
