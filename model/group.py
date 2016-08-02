22a1caa10df96e334d663c9ab34ea03005f97c75
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-02 19:37:45 +0300

    Небольшие доработки

--- model/group.py
+++ model/group.py
@@ -1,6 +1,6 @@
 
 class Group:
     def __init__(self, name=None, header=None, footer=None):
-        self.group_name = name
+        self.name = name
         self.header = header
         self.footer = footer
--- test/add_new_group.py
+++ test/add_new_group.py
@@ -3,12 +3,10 @@ from model.group import Group
 
 
 def test_add_new_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test_group", header="qwerty", footer="qwerty"))
-    app.session.logout()
+
 
 def test_add_new_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
 
