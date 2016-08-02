6e6014b89b7b80678b4a5ed1625403b4e923b145
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-02 19:23:42 +0300

    Модифицирован тест, добавлены новые методы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,12 @@
 __author__ = 'ichistov'
+from model.group import Group
 
-def test_modify_first_group(app):
+def test_modify_first_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group()
+    app.group.modify_first_group(Group(name="new group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
