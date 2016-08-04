bf64180846c42af103f05caef1ae975d6e51a07a
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-04 19:22:10 +0300

    Добавлены предусловия на создание группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,11 @@ __author__ = 'ichistov'
 from model.group import Group
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test1'))
     app.group.modify_first_group(Group(name="new group"))
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test2'))
     app.group.modify_first_group(Group(header="New header"))
