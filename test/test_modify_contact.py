2c6488043128dbf9c6ebbf423038e32da840cffe
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-16 17:34:51 +0300

    Добавлены методы для удаления и модификации случайных контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,16 +1,18 @@
 __author__ = 'ichistov'
 from model.contact import Contact
+from random import randrange
 
 def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname='test_contact'))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(firstname="New contact")
-    contact.lastname = old_contacts[0].lastname
-    app.contact.modify_first_contact()
+    contact.lastname = old_contacts[index].lastname
+    app.contact.modify_contact_by_index(index, contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.lastname_or_max) == sorted(new_contacts, key=Contact.lastname_or_max)
 
 
