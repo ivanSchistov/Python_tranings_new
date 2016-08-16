59d268cca4ef30d10400aa6a21abed5e032c5746
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-16 12:31:01 +0300

    Добавлены проверки длины списков и сравнение списков имен контактов

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -13,18 +13,30 @@ def app(request):
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
     app.open_contact_page()
-    app.create(Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='John', middlename='S', lastname='Smith', nickname='JS', title='QQ', company='altego',
                        address='123', home='12456', mobile='123', email='123@ro.ru', homepage='www.site.com', bday='10',
                        bmonth='April', byear='1990', work='123', fax='1456987', email2='dad@gmail.com',
-                       email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!'))
+                       email3='dad1@gmail.com', aday='10', amonth='May', ayear='1989', address2='gadf12', phone2='123', notes='Hello!')
+    app.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.lastname_or_max) == sorted(new_contacts, key=Contact.lastname_or_max)
     app.session.logout()
 
 def test_add_new_empty_contact(app):
     app.session.login(username="admin", password="secret")
     app.open_contact_page()
-    app.create(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
                        address='', home='', mobile='', email='', homepage='', bday='',
                        bmonth='', byear='', work='', fax='', email2='',
-                       email3='', aday='', amonth='', ayear='', address2='', phone2='', notes=''))
+                       email3='', aday='', amonth='', ayear='', address2='', phone2='', notes='')
+    app.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.lastname_or_max) == sorted(new_contacts, key=Contact.lastname_or_max)
     app.session.logout()
 
