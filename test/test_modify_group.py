a5742a2972387662151a2415306e51326461cf79
ivanSchistov <ivan.s.chistov@gmail.com>
2016-08-16 11:02:10 +0300

    Добавлены сравнения списков для групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,9 +5,13 @@ def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test1'))
     old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
     app.group.modify_first_group(Group(name="new group"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
